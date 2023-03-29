9a3ddb0c70e222d733afe1b4007906ccaf5470c9
Jeon208 <testest1275@gmail.com>
2023-03-29 23:03:19 +0900

    v1

--- drawer.py
+++ drawer.py
@@ -1,9 +1,17 @@
+# Lotto Number Drawer
 import random
 
+
+
 num = int(input("[!] largest number of balls *ex 50\n>> "))
+
 num2 = int(input("[!] the number of balls *ex 6\n>> "))
+
 tries = int(input("[!] try number\n>> "))
+
 balls = range(1,num+1)
+
+
 for i in range(1,tries+1) :
     print("try :",i)
     print(random.sample(balls,num2))
