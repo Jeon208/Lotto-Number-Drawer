c751dd321aefbd35308dd21891e6f7f6c6915d67
Jeon208 <testest1275@gmail.com>
2023-03-29 22:48:37 +0900

    v1

---
+++ drawer.py
@@ -0,0 +1,10 @@
+import random
+
+num = int(input("[!] largest number of balls *ex 50\n>> "))
+num2 = int(input("[!] the number of balls *ex 6\n>> "))
+tries = int(input("[!] try number\n>> "))
+balls = range(1,num+1)
+for i in range(1,tries+1) :
+    print("try :",i)
+    print(random.sample(balls,num2))
+    print()
